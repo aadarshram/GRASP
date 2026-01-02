abc5d9e7950f2f64f577d86c9ee2fa0714f12827
M Mothish <cs24b033@smail.iitm.ac.in>
2026-01-02 16:24:19 +0530

    modified:   scripts/train_metaworld.sh

--- scripts/train_metaworld.sh
+++ scripts/train_metaworld.sh
@@ -50,6 +50,8 @@ deepspeed --master_port 29600 --num_gpus=1 --num_nodes=1 "$SCRIPT_DIR/train.py" 
   --mm_use_im_patch_token False \
   --image_aspect_ratio pad \
   --group_by_modality_length False \
+  # --bf16 True \
+  --fp16 True \
   --output_dir $OUTPUT \
   --max_steps 4000 \
   --per_device_train_batch_size 50 \
@@ -62,6 +64,7 @@ deepspeed --master_port 29600 --num_gpus=1 --num_nodes=1 "$SCRIPT_DIR/train.py" 
   --warmup_ratio 0.005 \
   --lr_scheduler_type "cosine" \
   --logging_steps 1 \
+  # --tf32 False \
   --model_max_length 2048 \
   --gradient_checkpointing True \
   --dataloader_num_workers 4 \
